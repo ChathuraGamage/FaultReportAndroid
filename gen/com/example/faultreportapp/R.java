c22d0edf13a05100e663c33e87fef94c55977cf6
chathuragamage <chathurapgamage@gmail.com>
2013-09-08 18:51:20 +0530

    Start Activity created

--- gen/com/example/faultreportapp/R.java
+++ gen/com/example/faultreportapp/R.java
@@ -24,7 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Button01=0x7f080002;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080001;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_start=0x7f030000;
@@ -33,8 +36,11 @@ public final class R {
         public static final int start=0x7f070000;
     }
     public static final class string {
+        public static final int Incident_button=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int app_start=0x7f050003;
+        public static final int fault_button=0x7f050005;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
@@ -64,5 +70,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int pop_up=0x7f060002;
     }
 }
